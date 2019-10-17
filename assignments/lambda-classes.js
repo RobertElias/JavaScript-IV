b6801a9afa23d5c7ca6c1a3c4075e2d2047114a4
Robert <Robert.Elias@live.com>
2019-10-17 13:32:32 -0700

    new Person constructor created to test Person Class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -2,24 +2,36 @@
 
 //Person Class
 
-class Person{
-    constructor(att) {
-      this.name = att.name;
-      this.age = att.age;
-      this.location = att.location;
-    }
-    speak() {
-      return `Hello my name is ${this.name} and I am from ${this.location}.`
-    }
+class Person {
+  constructor(att) {
+    this.name = att.name;
+    this.age = att.age;
+    this.location = att.location;
   }
+  speak() {
+    return `Hello my name is ${this.name} and I am from ${this.location}.`
+  }
+}
 
 
-  const Robert = new Person({
-      name: 'Robert Elias',
-      age: 43,
-      location: 'Orange County California'
-  })
 
-  //Testing Person Class
-  console.log(`new Person is from ${Robert.location}`);
-  console.log(`new Person is name ${Robert.name}`);
+//Person constructor
+const Robert = new Person({
+  name: 'Robert Elias',
+  age: 43,
+  location: 'Orange County California'
+})
+//Person constructor
+const Cynthia = new Person({
+  name: 'Cynthia Elias',
+  age: 39,
+  location: 'Los Angeles California'
+
+})
+
+//Testing Person Class
+console.log(`new Person is name ${Robert.name}`);
+console.log(`new Person is from ${Robert.location}`);
+
+console.log(`new Person is name ${Cynthia.name}`);
+console.log(`new Person is from ${Cynthia.location}`);
