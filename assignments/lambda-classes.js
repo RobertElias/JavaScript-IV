c5b4caf1035b6b5d5f6438210bbc743ce6b09fce
Robert <Robert.Elias@live.com>
2019-10-17 13:47:00 -0700

    Completed Intructor Class and tested fred

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -13,6 +13,23 @@ class Person {
   }
 }
 
+//Instructor class 
+
+class  Instructor extends Person{
+  constructor(att){
+    super(att);
+    this.specialty = att.specialty;
+    this.favLanguage = att.favLanguage;
+    this.catchPhrase = att.catchPhrase;
+  }
+  demo(subject){
+    return `Today we are learning ${subject}`;
+  }
+  grade(student){
+    return `${student.name} receives a perfect score on ${subject}.`;
+  }
+}
+
 
 
 //Person constructor
@@ -29,9 +46,26 @@ const Cynthia = new Person({
 
 })
 
+
+//Instructor  constructor
+
+const fred = new Instructor({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+});
+
 //Testing Person Class
 console.log(`new Person is name ${Robert.name}`);
 console.log(`new Person is from ${Robert.location}`);
 
 console.log(`new Person is name ${Cynthia.name}`);
 console.log(`new Person is from ${Cynthia.location}`);
+
+//Testing Instructor Class
+
+console.log(fred);
+console.log(fred.speak());
