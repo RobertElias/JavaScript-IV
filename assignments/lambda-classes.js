17fffbe87fe83f7a5f23af9d8f14fb96ed6442c3
Robert <Robert.Elias@live.com>
2019-10-17 13:12:20 -0700

    Person Class and constructor built

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,25 @@
 // CODE here for your Lambda Classes
+
+//Person Class
+
+class Person{
+    constructor(att) {
+      this.name = att.name;
+      this.age = att.age;
+      this.location = att.location;
+    }
+    speak() {
+      return `Hello my name is ${this.name} and I am from ${this.location}.`
+    }
+  }
+
+
+  const Robert = new Person({
+      name: 'Robert Elias',
+      age: 43,
+      location: 'Orange County California'
+  })
+
+  //Testing Person Class
+  console.log(`new Person is from ${Robert.location}`);
+  console.log(`new Person is name ${Robert.name}`);
